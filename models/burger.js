09e7897d042bcd048f17a5946e08bd739d95e933
chuman <42072991+chrishuman0923@users.noreply.github.com>
2018-11-20 14:47:17 -0500

    Adds function map to burger.js

---
+++ models/burger.js
@@ -0,0 +1,29 @@
+//Import ORM
+const orm = require('../config/orm.js');
+
+//Function to handle identical callbacks from orm functions
+function callBack(data, cb) {
+    cb(data);
+}
+
+//Create key-function pairs on burger object
+var burger = {
+    all: function(cb) {
+        orm.selectAll('burgers', '*', function(err, data) {
+            callBack(data, cb);
+        });
+    },
+    insert: function(name, cb) {
+        orm.insertOne('burgers', { burger_name: name }, function(err, data) {
+            callBack(data, cb);
+        });
+    },
+    update: function(name, id, cb) {
+        orm.updateOne('burgers', 'burger_name', name, 'id', id, function(err, data) {
+            callBack(data, cb);
+        });
+    }
+};
+
+//Export burger object
+module.exports = burger;
